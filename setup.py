d8e39bd0c442824bb9297289a5988fb526daa067
alemassaad <115092467+alemassaad@users.noreply.github.com>
2022-11-27 02:34:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-# read the contents of the README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-    
 setup(
     name='d-star-lite',
     version='1.0',
